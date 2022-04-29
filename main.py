5df8bec63a36bcb747cd61306984ace653d262a3
mysticBel <dnxbel@gmail.com>
2022-04-28 19:20:40 -0500

    calculate monthly_sales and total_sales

--- main.py
+++ main.py
@@ -9,15 +9,18 @@ def read_data():
             data.append(row)
     return data
 
+# Collect all of the sales from each month into a single list
+def calculate_sales(data):
+    total_sales = []
+    for row in data:
+        monthly_sales = row['sales']
+        total_sales.append(int(monthly_sales))
+
+    print(f'The total sales across all {len(total_sales)} months are {sum(total_sales)}.')
+    return len(total_sales), sum(total_sales)
+
 def run():
     data = read_data()
-
-    sales = []
-    for row in data:
-        sale = int(row['sales'])
-        sales.append(sale)
-
-    total = sum(sales)
-    print('Total Sales:{}'.format(total))
+    calculate_sales(data)
 
 run()
